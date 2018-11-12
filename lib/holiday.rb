ec4989ce10883e814e7f05e163271f96647b9441
Albina Zakirova <zak1804@gmail.com>
2018-11-12 00:04:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,6 +86,8 @@ def all_holidays_with_bbq(holiday_hash)
       end
     end
   end
+  bbq_holidays_supl
+end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
